47e42e3896525a72b7d873a75451a13d41d5cc91
ngr109 <sajjadhonarvar1@gmail.com>
2024-06-10 21:28:12 +0330

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,10 @@
+﻿// JavaScript source code
+alert("هنرجوی عزیز به سایت فروشگاه نگارآوا خوش امدید");
+function myfunction() {
+    var txt;
+    if (confirm("به سبدتون اضافه شد")) {
+        txt = "با موفقیت انجام شد";
+    } else {
+        txt = "انجام نشد";
+    }
+}
